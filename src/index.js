0b72aece26925ee26e30ace63095807cd5975a07
pkindalov <panayotkindalov@gmail.com>
2018-07-24 11:21:58 +0300

    Added redux logger middleware for more easily follow change of the state from the console.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 // import CardList from './CardList'
 import { searchRobots } from './reducers'
 import App from './containers/App'
@@ -11,7 +12,8 @@ import registerServiceWorker from './registerServiceWorker'
 
 // import { robots } from './robots'
 
-const store = createStore(searchRobots)
+const logger = createLogger()
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store}>
